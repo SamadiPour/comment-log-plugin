959f80c96c65dc268ea6004276df236437c4dbc2
GitHub Action <action@github.com>
2021-02-13 12:16:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/samadipour/commentlogplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.samadipour.commentlogplugin.services
+
+import com.github.samadipour.commentlogplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
